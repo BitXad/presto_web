290f1e6c4d132930865b24decb20f44f47f5237a
alejandro <ale.hv10@gmail.com>
2019-04-18 10:36:28 -0400

    credito

--- application/views/credito/individual.php
+++ application/views/credito/individual.php
@@ -1,6 +1,7 @@
 <script src="<?php echo base_url('resources/js/jquery-2.2.3.min.js'); ?>" type="text/javascript"></script>
 <script src="<?php echo base_url('resources/js/nuevocredito.js'); ?>"></script>
 <script src="<?php echo base_url('resources/js/index_individual.js'); ?>"></script>
+<link rel="stylesheet" href="<?php echo site_url('resources/css/Admin.css');?>">
 <!----------------------------- script buscador --------------------------------------->
 
 <script type="text/javascript">
@@ -15,15 +16,33 @@
                 })
             }(jQuery));
         });
+
 </script>
+
 <script type="text/javascript">
 function ocultar(){
                
                      var value = document.getElementById('modo').value;
+                     var limite = document.getElementById('credito_fechalimite').value;
+                     var today = new Date();
+                     //var ultimo = moment(limite).format("YYYY-MM-DD");
+                     //var hoy = moment(today).format("YYYY-MM-DD");
+
+                     var diff = moment(limite).diff(moment(today), 'days');
                      if (value==1) {
-                    $('#credito_cuotas').hide();
-                   }else{
-                    $('#credito_cuotas').show();
+                      html = "";
+                      html += "Fecha Limite Credito";
+                      html2 = "";
+                      html2 += "<div class='input-group no-print'> <span class='input-group-addon'>Numero Cuotas:</span><input type='text' name='credito_cuotas' class='form-control' value="+Number(diff+1)+" id='credito_cuotas' readonly /></div>";
+                      $('#fechinga').html(html);
+                      $('#cuotas').html(html2);
+                      }else{
+                     html = "";
+                     html += "Fecha Primer Pago";
+                     html2 = "";
+                     html2 += "<div class='input-group no-print'> <span class='input-group-addon'>Numero Cuotas:</span><select name='credito_cuotas' id='credito_cuotas'class='form-control'  ><option value='0'>Sin Limite de Tiempo</option><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option><option value='6'>6</option><option value='7'>7</option><option value='8'>8</option><option value='9'>9</option><option value='10'>10</option><option value='11'>11</option><option value='12'>12</option><option value='13'>13</option><option value='14'>14</option><option value='15'>15</option><option value='16'>16</option><option value='17'>17</option><option value='18'>18</option><option value='19'>19</option><option value='20'>20</option><option value='21'>21</option><option value='22'>22</option><option value='23'>23</option><option value='24'>24</option><option value='25'>25</option><option value='26'>26</option><option value='27'>27</option><option value='28'>28</option><option value='29'>29</option><option value='30'>30</option><option value='31'>31</option><option value='32'>32</option><option value='33'>33</option><option value='34'>34</option><option value='35'>35</option><option value='36'>36</option></select></div>";
+                     $('#fechinga').html(html);
+                    $('#cuotas').html(html2);
                    }
                 
       }      
@@ -195,8 +214,8 @@ function ocultar(){
                  </div>
                  <div class="col-md-12">
                  	<?php  $date = date("Y-m-d"); $mod_date = strtotime($date."+ 1 months");?>
-                  <div class="input-group no-print"> <span class="input-group-addon">Fecha Pago:</span>
-                   <input type="date" name="credito_fechalimite" value="<?php echo date("Y-m-d",$mod_date); ?>" class=" form-control" id="credito_fechalimite" />
+                  <div class="input-group no-print"> <span class="input-group-addon" id="fechinga">Fecha Primer Pago:</span>
+                   <input type="date" name="credito_fechalimite" value="<?php echo date("Y-m-d",$mod_date); ?>" class=" form-control" id="credito_fechalimite" onchange="ocultar()"/>
                   </div>
                  </div>
                  <!--<div class="col-md-12">
@@ -205,8 +224,8 @@ function ocultar(){
                   </div>
                  </div>-->
                  
-                  <div class="col-md-12">
-                  <div class="input-group no-print"> <span class="input-group-addon">Cuotas:</span>
+                  <div class="col-md-12" id="cuotas">
+                  <div class="input-group no-print"> <span class="input-group-addon">Numero Cuotas:</span>
                    
                     <select name="credito_cuotas" id="credito_cuotas"class="form-control"  >
 <option value="0">Sin Limite de Tiempo</option>
