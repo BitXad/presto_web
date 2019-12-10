b49681d8c19eb5f9b5083c8ea69069fa62935439
alejandro <ale.hv10@gmail.com>
2019-12-10 17:06:55 -0400

    dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -56,7 +56,7 @@
 
             <div class="info-box-content">
               <span class="info-box-text">CREDITOS</span>
-              <span class="info-box-number">20<small> APROBADOS</small></span>
+              <span class="info-box-number"><?php echo sizeof($creditos); ?><small> APROBADOS</small></span>
             </div>
             <!-- /.info-box-content -->
           </div>
@@ -68,8 +68,8 @@
             <span class="info-box-icon bg-red"><i class="fa fa-user"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">Clientes</span>
-              <span class="info-box-number">25</span>
+              <span class="info-box-text">CLIENTES</span>
+              <span class="info-box-number"><?php echo sizeof($clientes); ?><small> REGISTRADOS</small></span>
             </div>
             <!-- /.info-box-content -->
           </div>
@@ -86,7 +86,7 @@
 
             <div class="info-box-content">
               <span class="info-box-text">GRUPOS</span>
-              <span class="info-box-number">8 ACTIVOS</span>
+              <span class="info-box-number"><?php echo sizeof($grupos); ?><small> ACTIVOS</small></span>
             </div>
             <!-- /.info-box-content -->
           </div>
@@ -99,7 +99,7 @@
 
             <div class="info-box-content">
               <span class="info-box-text">Asesores</span>
-              <span class="info-box-number">14</span>
+              <span class="info-box-number"><?php echo sizeof($asesores); ?><small> ACTIVOS</small></span>
             </div>
             <!-- /.info-box-content -->
           </div>
@@ -575,7 +575,7 @@
           <!-- TABLE: LATEST ORDERS -->
           <div class="box box-info">
             <div class="box-header with-border">
-              <h3 class="box-title">Latest Orders</h3>
+              <h3 class="box-title">Utimos Grupos</h3>
 
               <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
@@ -589,69 +589,24 @@
                 <table class="table no-margin">
                   <thead>
                   <tr>
-                    <th>Order ID</th>
-                    <th>Item</th>
-                    <th>Status</th>
-                    <th>Popularity</th>
+                    <th>Grupo ID</th>
+                    <th>Nombre</th>
+                    <th>Estado</th>
+                    <th>Miembros</th>
                   </tr>
                   </thead>
                   <tbody>
-                  <tr>
-                    <td><a href="pages/examples/invoice.html">OR9842</a></td>
-                    <td>Call of Duty IV</td>
-                    <td><span class="label label-success">Shipped</span></td>
+                    <?php foreach ($grupos20 as $c) {  ?>
+                      <tr>
+                    <td><a href="#"><?php echo $c["grupo_id"] ?></a></td>
+                    <td><?php echo $c["grupo_nombre"] ?></td>
+                    <td><span  style="border-radius: 50px;padding-left: 5px;padding-right: 5px; background: <?php echo $c["estado_color"] ?>"><?php echo $c["estado_descripcion"] ?></span></td>
                     <td>
-                      <div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63</div>
+                      <div class="sparkbar" data-color="#00a65a" data-height="20"><?php echo $c["grupo_integrantes"] ?></div>
                     </td>
                   </tr>
-                  <tr>
-                    <td><a href="pages/examples/invoice.html">OR1848</a></td>
-                    <td>Samsung Smart TV</td>
-                    <td><span class="label label-warning">Pending</span></td>
-                    <td>
-                      <div class="sparkbar" data-color="#f39c12" data-height="20">90,80,-90,70,61,-83,68</div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td><a href="pages/examples/invoice.html">OR7429</a></td>
-                    <td>iPhone 6 Plus</td>
-                    <td><span class="label label-danger">Delivered</span></td>
-                    <td>
-                      <div class="sparkbar" data-color="#f56954" data-height="20">90,-80,90,70,-61,83,63</div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td><a href="pages/examples/invoice.html">OR7429</a></td>
-                    <td>Samsung Smart TV</td>
-                    <td><span class="label label-info">Processing</span></td>
-                    <td>
-                      <div class="sparkbar" data-color="#00c0ef" data-height="20">90,80,-90,70,-61,83,63</div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td><a href="pages/examples/invoice.html">OR1848</a></td>
-                    <td>Samsung Smart TV</td>
-                    <td><span class="label label-warning">Pending</span></td>
-                    <td>
-                      <div class="sparkbar" data-color="#f39c12" data-height="20">90,80,-90,70,61,-83,68</div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td><a href="pages/examples/invoice.html">OR7429</a></td>
-                    <td>iPhone 6 Plus</td>
-                    <td><span class="label label-danger">Delivered</span></td>
-                    <td>
-                      <div class="sparkbar" data-color="#f56954" data-height="20">90,-80,90,70,-61,83,63</div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td><a href="pages/examples/invoice.html">OR9842</a></td>
-                    <td>Call of Duty IV</td>
-                    <td><span class="label label-success">Shipped</span></td>
-                    <td>
-                      <div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63</div>
-                    </td>
-                  </tr>
+                    <?php } ?>
+                  
                   </tbody>
                 </table>
               </div>
@@ -659,8 +614,8 @@
             </div>
             <!-- /.box-body -->
             <div class="box-footer clearfix">
-              <a href="javascript:void(0)" class="btn btn-sm btn-info btn-flat pull-left">Place New Order</a>
-              <a href="javascript:void(0)" class="btn btn-sm btn-default btn-flat pull-right">View All Orders</a>
+              <a href="javascript:void(0)" class="btn btn-sm btn-info btn-flat pull-left"></a>
+              <a href="javascript:void(0)" class="btn btn-sm btn-default btn-flat pull-right"></a>
             </div>
             <!-- /.box-footer -->
           </div>
